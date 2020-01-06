84dd9769e08d4fa709364337785c8562d0dc953a
Remsko <pinoit.remi@gmail.com>
2020-01-06 21:40:20 +0100

    feat(split-mmap): explicitly wrap mmap

--- srcs/malloc/malloc.c
+++ srcs/malloc/malloc.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/mman.h>
 #include "../incs/malloc.h"
 
 #include <sys/resource.h>
@@ -13,6 +14,20 @@
 #define PAGE_SIZE (getpagesize())
 #define PAGE_ALIGN(size) (ALIGN(size, PAGE_SIZE))
 
+void *mmap_wrapper(size_t allocation_size)
+{
+	void *allocation;
+
+	allocation = mmap(NULL, allocation_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	if (allocation == MAP_FAILED)
+	{
+		if (DEBUG)
+			perror("Could not mmap");
+		return NULL;
+	}
+	return (allocation);
+}
+
 void *add_chunk(size_t size)
 {
 	void *new_allocation;
@@ -21,7 +36,7 @@ void *add_chunk(size_t size)
 
 	total_required = MEMORY_ALIGN(size + CHUNK_SIZE);
 	allocation_size = PAGE_ALIGN(total_required);
-	if ((new_allocation = new_memory_allocation(allocation_size)) == NULL)
+	if ((new_allocation = mmap_wrapper(allocation_size)) == NULL)
 		return NULL;
 
 	t_chunk_metadata *chunk = (t_chunk_metadata *)new_allocation;
--- srcs/memory.c
+++ srcs/memory.c
@@ -1,4 +1,3 @@
-#include <sys/mman.h>
 #include "incs/malloc.h"
 
 /*
@@ -14,33 +13,20 @@ t_memory *get_memory_singletone(void)
 	return &m;
 }
 
-void push_memory_chunk(t_chunk_metadata *cm)
+t_chunk_metadata *push_memory_chunk(t_chunk_metadata *cm)
 {
 	t_memory *memory;
 
 	memory = memory_singletone();
 	cm->next = memory->all;
 	memory->all = cm;
+	return cm;
 }
 
-void push_new_memory_chunk(void *alloc, size_t alloc_size)
+t_chunk_metadata *push_new_memory_chunk(void *alloc, size_t alloc_size)
 {
 	t_chunk_metadata *cm;
 
 	cm = new_chunk_metadata(alloc, alloc_size);
 	return push_memory_chunk(cm);
-}
-
-void *new_memory_allocation(size_t allocation_size)
-{
-	void *allocation;
-
-	allocation = mmap(NULL, allocation_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-	if (allocation == MAP_FAILED)
-	{
-		if (DEBUG)
-			perror("Could not mmap");
-		return NULL;
-	}
-	return (allocation);
 }
