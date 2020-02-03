40729530616c2d5884ad24bbc0f8d411e8a778fa
Maxime CASEAUX <mcaseaux@e2r5p22.42.fr>
2020-02-03 16:06:36 +0100

    feat(realloc): started a new realloc implementation

--- srcs/malloc/realloc.c
+++ srcs/malloc/realloc.c
@@ -1,12 +1,26 @@
 #include "chunk.h"
 #include "align.h"
+#include "malloc.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
 
 void *realloc(void *ptr, size_t size)
 {
 	t_chunk *old_chunk;
 	size_t old_chunk_size;
 
-	//t_chunk *new_chunk = NULL;
+	t_chunk *new_chunk;
 	size_t new_chunk_size;
 
 	// Check entries
@@ -18,18 +32,18 @@ void *realloc(void *ptr, size_t size)
 	old_chunk_size = old_chunk->forward;
 
 	new_chunk_size = memory_align(size + sizeof(t_chunk));
-	if (new_chunk_size < size)
-		return NULL;
-
-	if (old_chunk_size > new_chunk_size)
-	{
-		return NULL;
-	}
+	// If the new size can but putted in the existing chunk, return the same ptr 
+	if (new_chunk_size <= old_chunk_size)
+		return ptr;
 
 	// Malloc new chunk
+	new_chunk = get_chunk_from_payload(malloc(new_chunk_size));
 
 	// Copy old chunk's datas
+	new_chunk = ft_memcpy((void *) new_chunk, (void *) old_chunk, old_chunk_size);
+	new_chunk->forward = new_chunk_size;
 
 	// Free old chunk
+	free(old_chunk + sizeof(t_chunk));
 	return NULL;
 }
