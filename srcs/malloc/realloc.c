c50f6f56a255e907390cf995f14b03dbe0b96f9d
Maxime CASEAUX <mcaseaux@e2r5p22.42.fr>
2020-02-03 12:38:06 +0100

    feat(realloc): started realloc

--- srcs/malloc/realloc.c
+++ srcs/malloc/realloc.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "malloc.h"
 #include <string.h>
 #include "debug.h"
@@ -15,4 +16,43 @@ void *realloc(void *ptr, size_t size)
 		; //memcpy(re, ptr, (new < old) ? new : old);
 	}
 	return re;
+=======
+#include "chunk.h"
+#include "malloc.h"
+#include "arena.h"
+#include "debug.h"
+
+void	*realloc(void *ptr, size_t size){
+	t_chunk *old_chunk;
+	size_t old_chunk_size;
+
+	t_chunk *new_chunk;
+	size_t new_chunk_size;
+
+	// Check entries
+	if (ptr == NULL || size <= 0)
+		return;
+	
+	// Format datas
+	old_chunk = get_chunk_from_payload(ptr);
+	old_chunk_size = old_chunk->forward;
+	
+	new_chunk_size = memory_align(size + sizeof(t_chunk));
+	if (new_chunk_size < size)
+		return NULL;
+	
+	if (old_chunk_size > new_chunk_size)
+	{
+		return;
+	}
+
+	// Malloc new chunk
+	
+	
+
+	// Copy old chunk's datas
+
+	// Free old chunk
+	
+>>>>>>> feat(realloc): started realloc
 }
