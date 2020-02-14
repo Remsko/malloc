f7572d1d933e403adf5d2971feaeee708b224b4e
Remsko <pinoit.remi@gmail.com>
2020-02-14 14:09:20 +0100

    remove debug

--- srcs/malloc/malloc.c
+++ srcs/malloc/malloc.c
@@ -2,49 +2,23 @@
 #include "chunk.h"
 #include "malloc.h"
 #include "arena.h"
+#include "libc.h"
 #include "ft_printf.h"
+#include <stdlib.h>
 
 pthread_mutex_t g_thread_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-char malloc_debug(void)
-{
-	extern char **environ;
-	char flags;
-	int i;
-
-	flags = 0;
-	if (environ != NULL)
-	{
-		i = 0;
-		while (environ[i] != NULL)
-		{
-			if (strcmp(environ[i],"MALLOC_SIZE=1") == 0)
-				flags |= 1;
-			if (strcmp(environ[i],"MALLOC_ORIGINAL_SIZE=1") == 0)
-				flags |= 1 << 1;
-			i++;
-		}
-	}
-	return flags;
-}
 
 void *malloc_unlocked(size_t size)
 {
 	t_heap **head;
 	t_chunk *chunk;
 	size_t chunk_size;
-	char options;
 
-	options = malloc_debug();
-	if (options & (1 << 1))
-		ft_printf("Malloc original size: %zu\n", size);
 	if (size == 0)
 		size = 1;
 	chunk_size = memory_align(size + sizeof(t_chunk));
 	if (chunk_size < size)
 		return NULL;
-	if (options & 1)
-		ft_printf("Malloc size: %zu\n", chunk_size);
 	head = get_arena_heap_by_size(chunk_size);
 	chunk = get_free_chunk(head, chunk_size);
 	if (chunk == NULL)
